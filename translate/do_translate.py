9b6a633b2558561dadb3a3c5b9b56128af1ddc92
Hunseo Son <2423aa@naver.com>
2023-11-10 02:48:41 +0900

    openai api langchain update 반영

--- translate/do_translate.py
+++ translate/do_translate.py
@@ -1,5 +1,5 @@
 from langchain.chat_models import ChatOpenAI
-from langchain import LLMChain
+from langchain.chains import LLMChain
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
